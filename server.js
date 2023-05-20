92004ba8df69975a05e2fa89d2c665eb48e345ed
unknown <ajanihalimat2020@gmail.com>
2023-05-20 07:17:31 +0100

    just practicing

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',(req, res)=>{
     res.send('Hello NODE API')
 })
 
+app.get('/blog',(req, res)=>{
+    res.send('Hello Blog, My name is Halimah')
+})
+
 app.listen(port,()=>{
     console.log(`Node API is running on port 2001`)
 })
